28fdf04bfa809f3270435c03cb2a4d2dcefefc9f
ri cky <ricki@ris-Mac-mini.local>
2024-09-17 23:03:46 -0700

    more port debugging

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 from seed import seed_database
 
-# from seed import seed_database
+ from seed import seed_database
 from models import db, connect_db, Work, Performance, Admin, Appearance
 from forms import UserAddForm, LoginForm, EditForm
 
@@ -22,10 +22,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
      os.environ.get('DATABASE_URL', 'postgresql://leah_user:VMrcPl0kd06Lk703K1ohdmSayrI84T7D@dpg-cr9uu056l47c73cv32qg-a/leah'))
     
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config['SQLALCHEMY_ECHO'] = False
+#app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+#app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 
 
 #toolbar = DebugToolbarExtension(app)
@@ -33,7 +33,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 app.app_context().push()
 connect_db(app)
 
-#seed_database()
+seed_database()
 
 
 # # # # # # # # # # # # # # # # BEFORE # # # # # # # # # # # # # # # # # # # # 
