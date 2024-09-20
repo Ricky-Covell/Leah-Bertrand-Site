5943e030d10e4e08f3b9c0641ca67aa281c727b2
ri cky <ricki@ris-Mac-mini.local>
2024-09-19 18:32:31 -0700

    even more port debugging

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ if __name__ == '__main__':
       app.run(host='0.0.0.0', port=10000)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    #  os.environ.get('DATABASE_URL', 'postgresql:///leah'))
      os.environ.get('DATABASE_URL', 'postgresql://leah_user:VMrcPl0kd06Lk703K1ohdmSayrI84T7D@dpg-cr9uu056l47c73cv32qg-a/leah'))
+    #  os.environ.get('DATABASE_URL', 'postgresql:///leah'))
     
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
